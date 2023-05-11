ee4cc3bd648367a28143dd56fb86f900b97d3721
Semyon Surkov <semyonsurkov@outlook.com>
2023-05-11 14:40:33 +0300

    Add calculator [ v .1 ]
    This version has too many bugs, I will fix it later.

---
+++ script.js
@@ -0,0 +1,101 @@
+let firstNumber;
+let secondNumber;
+let operator;
+let displayValue = '';
+let lastResult;
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const operate = (operator, firstNumber, secondNumber) => {
+  switch (operator) {
+    case '+':
+      return parseFloat(firstNumber) + parseFloat(secondNumber);
+      break;
+    case '-':
+      return parseFloat(firstNumber) - parseFloat(secondNumber);
+      break;
+    case '*':
+      return parseFloat(firstNumber) * parseFloat(secondNumber);
+      break;
+    case '/':
+      return parseFloat(firstNumber) / parseFloat(secondNumber);
+      break;
+  }
+};
+
+const numberButtons = document.querySelectorAll('.btn-number');
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (!operator) {
+      if (!firstNumber) {
+        firstNumber = button.value;
+      } else {
+        firstNumber += button.value;
+      }
+    } else {
+      if (!secondNumber && firstNumber) {
+        secondNumber = button.value;
+      } else if (secondNumber && firstNumber) {
+        return;
+      } else {
+        secondNumber += button.value;
+      }
+    }
+    updateDisplay(button.value);
+  });
+});
+
+const operatorsButtons = document.querySelectorAll('.btn-operator');
+
+operatorsButtons.forEach((operatorButton) => {
+  operatorButton.addEventListener('click', () => {
+    if (firstNumber && !operator) {
+      operator = operatorButton.value;
+      updateDisplay(operator);
+    }
+  });
+});
+
+function updateDisplay(value) {
+  displayValue += value;
+  const screenCurrent = document.querySelector('.screen-current');
+  screenCurrent.textContent = displayValue;
+}
+
+const resetButton = document.querySelector('.btn-clear');
+
+resetButton.addEventListener('click', () => {
+  firstNumber = null;
+  secondNumber = null;
+  operator = null;
+  displayValue = '';
+  const screenCurrent = document.querySelector('.screen-current');
+  screenCurrent.textContent = displayValue;
+  summary.innerHTML = '';
+});
+
+const equalButton = document.querySelector('.btn-equals');
+const summary = document.querySelector('.screen-result');
+
+equalButton.addEventListener('click', () => {
+  if (operator && firstNumber && secondNumber) {
+    const result = operate(operator, firstNumber, secondNumber);
+    updateDisplay('');
+    summary.innerHTML = result;
+  }
+});
